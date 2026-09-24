efdc3a10d59222b799514f5fbde67e7e7f306394
abdulsubhanbackup-jpg <abdulsubhan.backup@gmail.com>
2026-09-24 17:07:11 +0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,4 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins CI/CD!");
-    }
+System.out.println("Hello World from Jenkins!");    }
 }
